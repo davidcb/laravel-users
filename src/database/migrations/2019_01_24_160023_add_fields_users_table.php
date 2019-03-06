bd2ecaec0f87750d7e8913307516cc857a68e5b1
David Couto <davidcouto@gmail.com>
2019-03-06 14:21:17 +0000

    role_id can be null

--- src/database/migrations/2019_01_24_160023_add_fields_users_table.php
+++ src/database/migrations/2019_01_24_160023_add_fields_users_table.php
@@ -14,7 +14,7 @@ class AddFieldsUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('role_id')->unsigned();
+            $table->integer('role_id')->unsigned()->nullable();
 
             $table->foreign('role_id')->references('id')->on('roles');
         });        
